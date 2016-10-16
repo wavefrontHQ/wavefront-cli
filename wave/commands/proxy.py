07dbbf99b9ecd48bed32349d0fa6d560f2af2fc8
Evan Pease <evanpease@WFSE01MBPRO.local>
2016-10-15 18:23:32 -0700

    proxy impl

--- wave/commands/proxy.py
+++ wave/commands/proxy.py
@@ -29,21 +29,49 @@ class Proxy(Base):
         print "Detected ", dist
         if dist == "Amazon Linux AMI":
             cmd = "curl -s %s | sudo bash" % (Proxy.pkg_rpm)
-            cmd += "&& sudo yum -y -q install wavefront-proxy"
+            cmd += " && sudo yum -y -q install wavefront-proxy"
             return cmd
         elif dist == "Ubuntu":
             return Proxy.pkg_deb
         else:
             print "Error: Unsupported OS version: %s. Please contact support@wavefront.com." % (dist)
 
-    def run(self):
-        print 'Hello from proxy'
-        print 'Running proxy installer with the following options:', dumps(self.options, indent=2, sort_keys=True)
-
-        # Run packagecloud installation
-        print "Running proxy installation"
+    def install_proxy(self):
         cmd = self.get_install_cmd()
         print cmd
         ret_code = subprocess.call(cmd, shell=True)
         if ret_code > 0:
-            print "Error installing proxy. Please check the installation logs above this line."
+            print "Error installing proxy. Please check the installation output above this line."
+
+    def validate_token(self,url,token):
+        # /daemon/test?token=$TOKEN
+        
+
+    def configure_proxy(self,url,token):
+        print url
+        print token
+        # replace token
+        token_str = "\#token\=XXX"
+
+        cmd = 'sudo sed -i -e "s/%s/token=%s/g" /opt/wavefront/wavefront-proxy/conf/wavefront.conf' % (token_str,token)
+        subprocess.call(cmd, shell=True)
+
+        # replace server url
+        url_str = "https://try.wavefront.com/api/"
+        cmd = "sudo sed -i -e 's,%s,%s,g' /opt/wavefront/wavefront-proxy/conf/wavefront.conf" % (url_str,url)
+        subprocess.call(cmd, shell=True)
+
+        # restart proxy
+        ret_code = subprocess.call("sudo service wavefront-proxy restart", shell=True)
+
+
+    def run(self):
+
+        print 'Running proxy installer with the following options:', dumps(self.options, indent=2, sort_keys=True)
+        wf_url = self.options['--wavefront-url']
+        api_token = self.options['--api-token']
+        # Run packagecloud installation
+        print "Running proxy installation"
+        self.install_proxy()
+        print "Configuring proxy"
+        self.configure_proxy(wf_url,api_token)
