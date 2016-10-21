6877b7b0afc4981e991e7159954f58a11077ddda
Evan Pease <evanpease@WFSE01MBPRO.local>
2016-10-21 14:16:56 -0700

    agent install impl

--- wavefront_cli/commands/proxy.py
+++ wavefront_cli/commands/proxy.py
@@ -4,8 +4,8 @@ from .base import Base
 # wavefront API functions
 from wavefront.api import validate_token, clean_url
 import wavefront.auth
+import wavefront.system
 
-import requests
 import subprocess
 import json
 import os
@@ -18,34 +18,61 @@ class Proxy(Base):
     pkg_deb = "https://packagecloud.io/install/repositories/wavefront/proxy/script.deb.sh"
     pkg_rpm = "https://packagecloud.io/install/repositories/wavefront/proxy/script.rpm.sh"
 
+    '''
     def check_os(self):
         try:
             if platform.linux_distribution() == ('', '', ''):
                 #aws linux workaround
-                return platform.linux_distribution(supported_dists=['system'])[0]
+                if platform.linux_distribution(supported_dists=['system'])[0] != None:
+                    return platform.linux_distribution(supported_dists=['system'])[0]
             else:
                 return platform.linux_distribution[0]
         except:
             print "Unable to detect Linux distribution. ", sys.exc_info()
+    '''
+
+    def get_remove_cmd(self):
+        # dist = self.check_os()
+        dist = wavefront.system.check_os()
+        print "Detected ", dist
+        if dist == "Amazon Linux AMI":
+            cmd = "sudo yum -y remove wavefront-proxy"
+            return cmd
+        elif dist == "Ubuntu":
+            cmd = "sudo apt-get -y remove wavefront-proxy"
+            return cmd
+        else:
+            print "Error: Unsupported OS version: %s. Please contact support@wavefront.com." % (dist)
 
     def get_install_cmd(self):
-        dist = self.check_os()
+        # dist = self.check_os()
+        dist = wavefront.system.check_os()
         print "Detected ", dist
         if dist == "Amazon Linux AMI":
             cmd = "curl -s %s | sudo bash" % (Proxy.pkg_rpm)
             cmd += " && sudo yum -y -q install wavefront-proxy"
             return cmd
         elif dist == "Ubuntu":
-            return Proxy.pkg_deb
+            cmd = "curl -s %s | sudo bash" % (Proxy.pkg_deb)
+            cmd += " && sudo apt-get -y -q install wavefront-proxy"
+            return cmd
         else:
             print "Error: Unsupported OS version: %s. Please contact support@wavefront.com." % (dist)
 
-    def install_proxy(self):
-        cmd = self.get_install_cmd()
-        print cmd
+
+    def uninstall_proxy(self):
+        cmd = self.get_remove_cmd()
+        print "Running ", cmd
         ret_code = subprocess.call(cmd, shell=True)
         if ret_code > 0:
-            print "Error installing proxy. Please check the installation output above this line."
+            print "Error removing proxy. Please check the output above this message."
+
+    def install_proxy(self):
+        cmd = self.get_install_cmd()
+        print "Running ", cmd
+        ret_code = subprocess.call(cmd, shell=True)
+        if ret_code > 0:
+            print "Error installing proxy. Please check the installation output above this message."
 
     def configure_proxy(self,url,token):
         url = clean_url(url) + "/api/"
@@ -64,27 +91,26 @@ class Proxy(Base):
 
     def run(self):
 
-        print 'Running proxy installer with the following options:', dumps(self.options, indent=2, sort_keys=True)
-        # wf_url = self.options['--wavefront-url']
-        # api_token = self.options['--api-token']
-        creds = wavefront.auth.get_or_set_auth(self.options)
-        if creds == None:
-            print "Unable to obtain Wavefront API credentials."
+        print 'Running proxy installer'
+
+        if self.options['remove']:
+            self.uninstall_proxy()
             sys.exit(0)
+        elif self.options['install']:
+            creds = wavefront.auth.get_or_set_auth(self.options)
+            if creds == None:
+                print "Unable to obtain Wavefront API credentials."
+                sys.exit(0)
 
-        wf_url = creds['user_url']
-        api_token = creds['user_token']
-        # 1) Validate token
-        #valid_token = self.validate_token(wf_url,api_token)
-        valid_token = validate_token(wf_url, api_token)
-        if not valid_token:
-            sys.exit(0)
+            wf_url = creds['user_url']
+            api_token = creds['user_token']
 
+            # 1) Run packagecloud installation
+            print "Running proxy installation"
+            self.install_proxy()
 
-        # 2) Run packagecloud installation
-        print "Running proxy installation"
-        self.install_proxy()
-
-        # 3) Configure proxy
-        print "Configuring proxy"
-        self.configure_proxy(wf_url,api_token)
+            # 2) Configure proxy
+            print "Configuring proxy"
+            self.configure_proxy(wf_url,api_token)
+        else:
+            print 'You must pass either the "install" or "remove" flag to the proxy command. Run "wave -h" for more information.'
---
+++ wavefront_cli/commands/wavefront/system.py
@@ -0,0 +1,13 @@
+
+import platform
+
+def check_os():
+    try:
+        if platform.linux_distribution() == ('', '', ''):
+            # aws linux workaround
+            if platform.linux_distribution(supported_dists=['system'])[0] != None:
+                return platform.linux_distribution(supported_dists=['system'])[0]
+        else:
+            return platform.linux_distribution[0]
+    except:
+        print "Unable to detect Linux distribution. ", sys.exc_info()
