3060842fa3d4f2fd5da551d4bcaa19a15f002323
anwang-vmware <77298260+anwang-vmware@users.noreply.github.com>
2021-02-23 10:07:30 -0800

    PUB-290 Adding python version check (#25)
    PUB-290 adding python version check and install corresponding pip

--- sh/install.sh
+++ sh/install.sh
@@ -135,7 +135,12 @@ function remove_python() {
 
 function install_pip() {
     PYTHON_PATH=$1
-    curl -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py >> ${INSTALL_LOG} 2>&1
+    ver=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
+    if [ "$ver" -lt "30" ]; then
+        curl -o /tmp/get-pip.py https://bootstrap.pypa.io/2.7/get-pip.py >> ${INSTALL_LOG} 2>&1
+    elif [ "$ver" -ge "30" ]; then
+        curl -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py >> ${INSTALL_LOG} 2>&1
+    fi
     if [ $? -ne 0 ]; then
             exit_with_failure "Failed to download Pip"
     fi
