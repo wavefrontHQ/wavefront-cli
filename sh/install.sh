001eadc4b1e2650cb063adc4834f599bbcd650c0
anwang-vmware <77298260+anwang-vmware@users.noreply.github.com>
2021-03-15 16:53:11 -0700

    ESO-2568 Change URL that we used to fetch pip installation script (#27)
    * install pip 20.3.4
    
    * pip==20.3.4
    
    * check python version and install pip
    
    * adding python version check and install corresponding pip
    
    * adding python version check and install corresponding pip
    
    * adding python version check and install corresponding pip
    
    * change from gt to ge in install_pip function
    
    * change from python to PYTHON_PATH
    
    * adding python path with install.sh
    
    * instead of python
    
    * adding python path
    
    * update the url
    
    * clean the comment
    
    * change curl url
    
    * adding check
    
    * clean up comment
    
    * remove extra line before/after install_pip()
    
    Co-authored-by: Ajay Jain <32074182+ajayj89@users.noreply.github.com>

--- sh/install.sh
+++ sh/install.sh
@@ -132,12 +132,11 @@ function remove_python() {
 
 }
 
-
 function install_pip() {
     PYTHON_PATH=$1
     ver=$($PYTHON_PATH -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
     if [ "$ver" -lt "30" ]; then
-        curl -o /tmp/get-pip.py https://bootstrap.pypa.io/2.7/get-pip.py >> ${INSTALL_LOG} 2>&1
+        curl -o /tmp/get-pip.py https://bootstrap.pypa.io/pip/2.7/get-pip.py >> ${INSTALL_LOG} 2>&1
     elif [ "$ver" -ge "30" ]; then
         curl -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py >> ${INSTALL_LOG} 2>&1
     fi
