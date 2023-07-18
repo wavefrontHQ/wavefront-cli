9182cb0fee2fcc91e1c87ce510209e366d71dbca
Norayr Chaparyan <nchaparyan@vmware.com>
2023-07-18 21:07:49 +0400

    Fixed an issue related to opening wavefront.conf file. wavefront.conf file created in instalation time and shouldn't be opened before that.

--- wavefront_cli/commands/install.py
+++ wavefront_cli/commands/install.py
@@ -126,23 +126,11 @@ class Install(Base):  # pylint: disable=too-few-public-methods
 
             auth_type = False
             if csp_org_id and csp_app_id and csp_app_secret:
-                lib.configure_csp_oauth_options()
-                lib.comment_auth_methods(csp_api_token=True,
-                                         csp_oauth_app=False,
-                                         wavefront_api_token=True)
                 auth_type = True
             elif csp_api_token:
-                lib.configure_csp_api_token_options()
-                lib.comment_auth_methods(csp_api_token=False,
-                                         csp_oauth_app=True,
-                                         wavefront_api_token=True)
                 auth_type = True
             elif wavefront_api_token:
                 auth_type = True
-                lib.configure_wavefront_api_token_options()
-                lib.comment_auth_methods(csp_api_token=True,
-                                         csp_oauth_app=True,
-                                         wavefront_api_token=False)
                 print("Validating API Token using Wavefront URL: ",
                       wavefront_url)
                 if not lib.api.validate_token(wavefront_url,
@@ -157,6 +145,22 @@ class Install(Base):  # pylint: disable=too-few-public-methods
             # Install Proxy
             if not lib.proxy.install_proxy(proxy_next):
                 sys.exit(1)
+
+            if csp_org_id and csp_app_id and csp_app_secret:
+                lib.configure_csp_oauth_options()
+                lib.comment_auth_methods(csp_api_token=True,
+                                         csp_oauth_app=False,
+                                         wavefront_api_token=True)
+            elif csp_api_token:
+                lib.configure_csp_api_token_options()
+                lib.comment_auth_methods(csp_api_token=False,
+                                         csp_oauth_app=True,
+                                         wavefront_api_token=True)
+            elif wavefront_api_token:
+                lib.configure_wavefront_api_token_options()
+                lib.comment_auth_methods(csp_api_token=True,
+                                         csp_oauth_app=True,
+                                         wavefront_api_token=False)
 
             # Configure Proxy
             if not lib.proxy.configure_proxy(wavefront_url,
