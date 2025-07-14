133ae75887cb8508f3496910e3ebf050690094be
Joanna Ko <joanna.ko@broadcom.com>
2025-07-14 14:36:00 -0700

    Migrate from pkg_resources to importlib.metadata (#40)

--- wavefront_cli/__init__.py
+++ wavefront_cli/__init__.py
@@ -1,11 +1,11 @@
 """Initialize wavefront cli version."""
 
-import pkg_resources
+import importlib.metadata
 
 __version__ = None
 
 try:
-    __version__ = pkg_resources.get_distribution('wavefront-cli').version
-except pkg_resources.DistributionNotFound:
+    __version__ = importlib.metadata.version('wavefront-cli')
+except importlib.metadata.PackageNotFoundError:
     # __version__ is only available when the distribution is installed.
     pass
